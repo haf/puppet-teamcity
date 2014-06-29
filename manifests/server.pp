30dd36238c719e485e917f6b08ec13471f431d9e
Henrik <henrik@haf.se>
2014-06-29 16:32:19 +0200

    removing firewall from teamcity module, put that in your profile code instead

--- manifests/server.pp
+++ manifests/server.pp
@@ -7,8 +7,7 @@ class teamcity::server(
   $conf_dir        = '/opt/teamcity-server/conf',
   $port            = 8111,
   $server_opts     = '',
-  $server_mem_opts = '-Xms750m -Xmx750m -XX:MaxPermSize=270m',
-  $manage_firewall = hiera('manage_firewall', false)
+  $server_mem_opts = '-Xms750m -Xmx750m -XX:MaxPermSize=270m'
 ) {
   $service      = 'teamcity-server'
   $bin_dir      = "$home_dir/bin"
@@ -21,21 +20,15 @@ class teamcity::server(
 
   # TODO: set up external database
 
-  anchor { 'teamcity::server::start': }
-
   include teamcity::common
 
   user { $user:
     ensure => present,
     system => true,
-    require => Anchor['teamcity::server::start'],
-    before  => Anchor['teamcity::server::end'],
   }
 
   package { 'teamcity-server':
     ensure  => installed,
-    require => Anchor['teamcity::server::start'],
-    before  => Anchor['teamcity::server::end'],
   }
 
   class { 'teamcity::server::config':
@@ -50,25 +43,10 @@ class teamcity::server(
     status     => 'ps aux | grep /usr/bin/java | grep teamcity_logs',
     hasrestart => true,
     require    => [
-      Anchor['teamcity::server::start'],
       Class['java'],
       User[$user],
       Group[$teamcity::common::group],
       Package['teamcity-server']
     ],
-    before     => Anchor['teamcity::server::end'],
   }
-
-  if $manage_firewall {
-    firewall { "211 allow teamcity-server:8111":
-      proto   => 'tcp',
-      state   => ['NEW'],
-      dport   => 8111,
-      action  => 'accept',
-      require => Anchor['teamcity::server::start'],
-      before  => Anchor['teamcity::server::end'],
-    }
-  }
-
-  anchor { 'teamcity::server::end': }
 }
